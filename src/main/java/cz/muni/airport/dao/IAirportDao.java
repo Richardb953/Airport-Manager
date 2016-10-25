1feab57fb98047a5edc05983d60be37c6e0c200d
Jiri Krejci <xkrejci7@nymfe59.fi.muni.cz>
2016-10-25 14:48:45 +0200

    Airport entity after review

--- src/main/java/cz/muni/airport/dao/IAirportDao.java
+++ src/main/java/cz/muni/airport/dao/IAirportDao.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.airport.dao;
 
 import cz.muni.airport.model.Airport;
@@ -15,20 +10,20 @@ import java.util.Locale;
  */
 public interface IAirportDao {
 
-    public void create(Airport airport) throws IllegalArgumentException;
+    void create(Airport airport) throws IllegalArgumentException;
 
-    public void update(Airport airport) throws IllegalArgumentException;
+    void update(Airport airport) throws IllegalArgumentException;
 
-    public void remove(Airport airport) throws IllegalArgumentException;
+    void remove(Airport airport) throws IllegalArgumentException;
 
-    public Airport getAirport(Long id) throws IllegalArgumentException;
+    Airport getAirport(Long id) throws IllegalArgumentException;
 
-    public List<Airport> getAllAirports();
+    List<Airport> getAllAirports();
 
-    public List<Airport> getAirportsByCity(String city) throws IllegalArgumentException;
+    List<Airport> getAirportsByCity(String city) throws IllegalArgumentException;
 
-    public List<Airport> getAirportsByCountry(String country) throws IllegalArgumentException;
+    List<Airport> getAirportsByCountry(String country) throws IllegalArgumentException;
 
-    public List<Airport> getAirportsByCountry(Locale locale) throws IllegalArgumentException;
+    List<Airport> getAirportsByCountry(Locale locale) throws IllegalArgumentException;
 
 }
