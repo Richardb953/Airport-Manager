dbe9b6b9e1623b57509a996fa6e13bd9e8a8880d
Karolína Božková <karolina.bozkova@gmail.com>
2016-10-25 14:00:28 +0200

    added AirplaneDao and AirplaneDaoImpl

---
+++ src/main/java/cz/muni/airport/dao/AirplaneDao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.airport.dao;
+
+import cz.muni.airport.model.Airplane;
+import java.util.List;
+
+/**
+ *
+ * @author Karolína Božková
+ */
+public interface AirplaneDao {
+    public List<Airplane> getAllAirplanes();
+    public Airplane getAirplaneByName(String name);
+    
+    public void addAirplane(Airplane airplane);
+    public void updateAirplane(Airplane airplane);
+    public void removeAirplane(Airplane airplane);
+    
+}
---
+++ src/main/java/cz/muni/airport/dao/impl/AirplaneDaoImpl.java
@@ -0,0 +1,35 @@
+package cz.muni.airport.dao.impl;
+
+import cz.muni.airport.dao.AirplaneDao;
+import cz.muni.airport.model.Airplane;
+import java.util.List;
+
+
+public class AirplaneDaoImpl implements AirplaneDao {
+
+    @Override
+    public List<Airplane> getAllAirplanes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Airplane getAirplaneByName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addAirplane(Airplane airplane) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateAirplane(Airplane airplane) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeAirplane(Airplane airplane) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
