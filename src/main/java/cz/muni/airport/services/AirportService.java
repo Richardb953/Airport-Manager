9098ef964850169d33e0b84541643e4f86c0388d
= <=>
2016-10-29 19:18:45 +0200

    Airport service

--- src/main/java/cz/muni/airport/services/AirportService.java
+++ src/main/java/cz/muni/airport/services/AirportService.java
@@ -1,5 +1,7 @@
 package cz.muni.airport.services;
 
+import cz.muni.airport.model.Airport;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,6 +12,16 @@ import org.springframework.stereotype.Service;
 @Service
 public interface AirportService {
     
+    Airport saveAirport(Airport airport) throws IllegalArgumentException;
+    Airport updateAirport(Airport airport) throws IllegalArgumentException;
+    void removeAirport(Airport airport) throws IllegalArgumentException;
+
+    List<Airport> getAllAirports();
+    
+    Airport getAirportById(Long id) throws IllegalArgumentException;
+    List<Airport> getAirportsByCity(String city) throws IllegalArgumentException;
+    List<Airport> getAirportsByName(String name) throws IllegalArgumentException;
+    List<Airport> getAirportsByCountry(String country) throws IllegalArgumentException;   
     
     
 }
--- src/main/java/cz/muni/airport/services/impl/AirportServiceImpl.java
+++ src/main/java/cz/muni/airport/services/impl/AirportServiceImpl.java
@@ -3,14 +3,62 @@ package cz.muni.airport.services.impl;
 import cz.muni.airport.services.AirportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import cz.muni.airport.dao.AirportDAO;
+import cz.muni.airport.model.Airport;
+import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
- * @author Jirka
+ * @author Jiøí Krejèí
  */
 public class AirportServiceImpl implements AirportService {
     
     @Autowired
-    private AirportDAO airportDao;
+    private AirportDAO airportDAO;
+
+    @Override
+    @Transactional
+    public Airport saveAirport(Airport airport) {
+        airportDAO.addAirport(airport);
+        return airport;
+    }
+
+    @Override
+    @Transactional
+    public Airport updateAirport(Airport airport) {
+        airportDAO.updateAirport(airport);
+        return airport;
+    }
+
+    @Override
+    @Transactional
+    public void removeAirport(Airport airport) {
+        airportDAO.removeAirport(airport);
+    }
+
+    @Override
+    public List<Airport> getAllAirports() {
+        return airportDAO.getAllAirports();
+    }
+
+    @Override
+    public Airport getAirportById(Long id) {
+        return airportDAO.getAirportById(id);
+    }
+
+    @Override
+    public List<Airport> getAirportsByCity(String city) {
+        return airportDAO.getAirportsByCity(city);
+    }
+
+    @Override
+    public List<Airport> getAirportsByName(String name) {
+        return airportDAO.getAirportsByName(name);
+    }
+
+    @Override
+    public List<Airport> getAirportsByCountry(String country) {
+        return airportDAO.getAirportsByCountry(country);
+    }
 
 }
