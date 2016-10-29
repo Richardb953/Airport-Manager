a4f1164bcd9cf4bda201a96daf3cd5bca0db3e52
Karolína Božková <karolina.bozkova@gmail.com>
2016-10-29 14:44:44 +0200

    implemented AirplaneDAO and AirplaneService + Transactional annotation

---
+++ src/main/java/cz/muni/airport/dao/impl/AirplaneDAOImpl.java
@@ -0,0 +1,44 @@
+package cz.muni.airport.dao.impl;
+
+import cz.muni.airport.model.Airplane;
+import java.util.List;
+import cz.muni.airport.dao.AirplaneDAO;
+import org.springframework.stereotype.Repository;
+import cz.muni.airport.database.Connection;
+
+@Repository("airplaneDAO")
+public class AirplaneDAOImpl extends Connection implements AirplaneDAO {
+
+    @Override
+    public List<Airplane> getAllAirplanes() {
+        return (List<Airplane>) getHibernateTemplate().find("from Airplane ");
+    }
+    
+    @Override
+    public Airplane getAirplaneById(Long id) {
+        return getHibernateTemplate().get(Airplane.class, id);
+    }
+    
+    @Override
+    public List<Airplane> getAirplaneByName(String name) {
+        return (List<Airplane>) getHibernateTemplate().find("from Airplane where name = '" +name+"'");
+    }
+
+    @Override
+    public Airplane addAirplane(Airplane airplane) {
+        getHibernateTemplate().save(airplane);
+        return airplane;
+    }
+
+    @Override
+    public Airplane updateAirplane(Airplane airplane) {
+        getHibernateTemplate().update(airplane);
+        return airplane;
+    }
+
+    @Override
+    public void removeAirplane(Airplane airplane) {
+        getHibernateTemplate().delete(airplane);
+    }
+    
+}
---
+++ src/main/java/cz/muni/airport/services/impl/AirplaneServiceImpl.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.airport.services.impl;
+
+import cz.muni.airport.dao.AirplaneDAO;
+import cz.muni.airport.model.Airplane;
+import cz.muni.airport.services.AirplaneService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("airplaneService")
+public class AirplaneServiceImpl implements AirplaneService {
+    @Autowired
+    AirplaneDAO airplaneDAO;
+
+    public void setAirplaneDAO(AirplaneDAO airplaneDAO) {
+        this.airplaneDAO = airplaneDAO;
+    }
+   
+    @Override
+    @Transactional
+    public Airplane saveAirplane(Airplane airplane) {
+        airplaneDAO.addAirplane(airplane);
+        return airplane;
+    }
+
+    @Override
+    @Transactional
+    public Airplane updateAirplane(Airplane airplane) {
+        airplaneDAO.updateAirplane(airplane);
+        return airplane;
+    }
+
+    @Override
+    @Transactional
+    public void removeAirplane(Airplane airplane) {
+        airplaneDAO.removeAirplane(airplane);
+    }
+
+    @Override
+    public Airplane getAirplaneById(Long id) {
+        return airplaneDAO.getAirplaneById(id);
+    }
+
+    @Override
+    public List<Airplane> getAirplaneByName(String name) {
+        return airplaneDAO.getAirplaneByName(name);
+    }
+
+    @Override
+    public List<Airplane> getAllAirplanes() {
+        return airplaneDAO.getAllAirplanes();
+    }
+
+    
+    
+}
