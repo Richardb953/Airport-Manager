2c546d906e984fa2667a434a1b64675eff0ac1fa
Jiri Krejci <jirikr@gmail.com>
2016-10-31 00:51:12 +0100

    Update FlightDAOImpl.java

--- src/main/java/cz/muni/airport/dao/impl/FlightDAOImpl.java
+++ src/main/java/cz/muni/airport/dao/impl/FlightDAOImpl.java
@@ -6,7 +6,6 @@ import cz.muni.airport.model.Flight;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author rba on 23.10.2016.
@@ -15,39 +14,33 @@ import org.springframework.transaction.annotation.Transactional;
 public class FlightDAOImpl extends Connection implements FlightDAO {
 
     @Override
-    @Transactional
     public Flight addFlight(Flight flight) {
         getHibernateTemplate().save(flight);
         return flight;
     }
 
     @Override
-    @Transactional
     public Flight getFlight(Long id) {
         return getHibernateTemplate().get(Flight.class, id);
     }
 
     @Override
-    @Transactional
     public Flight updateFlight(Flight flight) {
         getHibernateTemplate().update(flight);
         return flight;
     }
 
     @Override
-    @Transactional
     public void removeFlight(Flight flight) {
         getHibernateTemplate().delete(flight);
     }
 
     @Override
-    @Transactional
     public List<Flight> getFlightsByName(String name) throws IllegalArgumentException {
         return (List<Flight>) getHibernateTemplate().findByNamedQueryAndNamedParam("Flight.findByName", "name", name);
     }
 
     @Override
-    @Transactional
     public List<Flight> getAllFlights() {
         return (List<Flight>) getHibernateTemplate().findByNamedQuery("Flight.findAll");
     }
