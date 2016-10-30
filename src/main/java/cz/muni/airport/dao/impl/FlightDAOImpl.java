ec98d1797dde575d9ce2b869f4bcd0a459ffeb30
Jiri Krejci <jirikr@gmail.com>
2016-10-31 00:52:08 +0100

    Update FlightDAOImpl.java

--- src/main/java/cz/muni/airport/dao/impl/FlightDAOImpl.java
+++ src/main/java/cz/muni/airport/dao/impl/FlightDAOImpl.java
@@ -10,38 +10,33 @@ import java.util.List;
 /**
  * @author rba on 23.10.2016.
  */
+
 @Repository("flightDAO")
 public class FlightDAOImpl extends Connection implements FlightDAO {
 
-    @Override
-    public Flight addFlight(Flight flight) {
+    @Override public Flight addFlight(Flight flight) {
         getHibernateTemplate().save(flight);
         return flight;
     }
 
-    @Override
-    public Flight getFlight(Long id) {
+    @Override public Flight getFlight(Long id) {
         return getHibernateTemplate().get(Flight.class, id);
     }
 
-    @Override
-    public Flight updateFlight(Flight flight) {
+    @Override public Flight updateFlight(Flight flight) {
         getHibernateTemplate().update(flight);
         return flight;
     }
 
-    @Override
-    public void removeFlight(Flight flight) {
+    @Override public void removeFlight(Flight flight) {
         getHibernateTemplate().delete(flight);
     }
 
-    @Override
-    public List<Flight> getFlightsByName(String name) throws IllegalArgumentException {
+    @Override public List<Flight> getFlightsByName(String name) throws IllegalArgumentException {
         return (List<Flight>) getHibernateTemplate().findByNamedQueryAndNamedParam("Flight.findByName", "name", name);
     }
 
-    @Override
-    public List<Flight> getAllFlights() {
+    @Override public List<Flight> getAllFlights() {
         return (List<Flight>) getHibernateTemplate().findByNamedQuery("Flight.findAll");
     }
 }
