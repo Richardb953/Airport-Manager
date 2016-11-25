77f4d731e3c4571abf6007a266191861555a62e4
Andrea N <andy.navratilova@gmail.com>
2016-11-25 22:30:07 +0100

    airport facade, services increment

---
+++ airport-facade/src/test/java/cz/muni/airport/facade/impl/AirportFacadeImplTest.java
@@ -0,0 +1,244 @@
+package cz.muni.airport.facade.impl;
+
+import cz.muni.airport.dto.AirportDTO;
+import cz.muni.airport.model.Airport;
+import cz.muni.airport.services.AirportService;
+import cz.muni.airport.services.BeanMappingService;
+import cz.muni.airport.services.impl.AirportServiceImpl;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import static org.mockito.Matchers.anyLong;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ *
+ * @author Andrea Navratilova, github name: andrea-n
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AirportFacadeImplTest {
+	@Mock
+    private AirportService airportService;
+
+    @Mock
+    private BeanMappingService beanMapperService;
+
+    @InjectMocks
+    private AirportFacadeImpl airportFacade = new AirportFacadeImpl();
+
+	public AirportFacadeImplTest() {
+	}
+
+	private AirportDTO createAirportDTO1(){
+        AirportDTO airportDTO = new AirportDTO();
+        airportDTO.setCity("Brno");
+		airportDTO.setIata("BRN");
+		airportDTO.setCountry("Czech republic");
+		airportDTO.setName("Brno letiste");
+		return airportDTO;
+    }
+    private Airport createAirport1(){
+        Airport airport = new Airport();
+        airport.setCity("Brno");
+		airport.setIata("BRN");
+		airport.setCountry("Czech republic");
+		airport.setName("Brno letiste");
+		return airport;
+    }
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	/**
+	 * Test of createAirport method, of class AirportFacadeImpl.
+	 */
+	@Test
+	public void testCreateAirport() {
+		System.out.println("createAirport");
+
+		Airport airport = createAirport1();
+		AirportDTO airportDTO = createAirportDTO1();
+
+		when(beanMapperService.mapTo(airportDTO, Airport.class)).thenReturn(airport);
+		when(beanMapperService.mapTo(airport, AirportDTO.class)).thenReturn(airportDTO);
+		when(airportService.saveAirport(airport)).thenReturn(airport);
+
+		AirportDTO created = airportFacade.createAirport(airportDTO);
+		assertEquals(airportDTO, created);
+	}
+
+	/**
+	 * Test of getAllAirports method, of class AirportFacadeImpl.
+	 */
+	@Test
+	public void testGetAllAirports() {
+		System.out.println("getAllAirports");
+		Airport airport = createAirport1();
+		AirportDTO airportDTO = createAirportDTO1();
+		
+		List<Airport> airports = new ArrayList<>();
+		airports.add(airport);
+
+		when(beanMapperService.mapTo(airportDTO, Airport.class)).thenReturn(airport);
+		when(beanMapperService.mapTo(airport, AirportDTO.class)).thenReturn(airportDTO);
+		when(airportService.findAllAirports()).thenReturn(airports);
+
+		List<AirportDTO> result = airportFacade.getAllAirports(); 
+		
+		verify(beanMapperService).mapTo(airports, AirportDTO.class);
+		verify(airportService).findAllAirports();
+	}
+
+	/**
+	 * Test of getAirportById method, of class AirportFacadeImpl.
+	 */
+	@Test
+	public void testGetAirportById() {
+		System.out.println("getAirportById");
+		Airport airport = createAirport1();
+		AirportDTO airportDTO = createAirportDTO1();
+
+		when(beanMapperService.mapTo(airportDTO, Airport.class)).thenReturn(airport);
+		when(beanMapperService.mapTo(airport, AirportDTO.class)).thenReturn(airportDTO);
+		when(airportService.findAirportById(anyLong())).thenReturn(airport);
+		
+		AirportDTO result = airportFacade.getAirportById(Long.MIN_VALUE);
+		verify(beanMapperService).mapTo(airport, AirportDTO.class);
+		verify(airportService).findAirportById(Long.MIN_VALUE);
+	}
+
+	/**
+	 * Test of getAirportsByIata method, of class AirportFacadeImpl.
+	 */
+	@Test
+	public void testGetAirportsByIata() {
+		System.out.println("getAirportsByIata");
+		Airport airport = createAirport1();
+		AirportDTO airportDTO = createAirportDTO1();
+
+		List<Airport> airports = new ArrayList<>();
+		airports.add(airport);
+		String iata = "BRN";
+
+		when(beanMapperService.mapTo(airportDTO, Airport.class)).thenReturn(airport);
+		when(beanMapperService.mapTo(airport, AirportDTO.class)).thenReturn(airportDTO);
+		when(airportService.findAirportByIata(iata)).thenReturn(airports);
+
+		List<AirportDTO> result = airportFacade.getAirportsByIata(iata);
+
+		verify(beanMapperService).mapTo(airports, AirportDTO.class);
+		verify(airportService).findAirportByIata(iata);
+	}
+
+	/**
+	 * Test of getAirportsByCity method, of class AirportFacadeImpl.
+	 */
+	@Test
+	public void testGetAirportsByCity() {
+		System.out.println("getAirportsByCity");
+		Airport airport = createAirport1();
+		AirportDTO airportDTO = createAirportDTO1();
+
+		List<Airport> airports = new ArrayList<>();
+		airports.add(airport);
+
+		String city = "Brno";
+
+		when(beanMapperService.mapTo(airportDTO, Airport.class)).thenReturn(airport);
+		when(beanMapperService.mapTo(airport, AirportDTO.class)).thenReturn(airportDTO);
+		when(airportService.findAirportsByCity(city)).thenReturn(airports);
+
+		List<AirportDTO> result = airportFacade.getAirportsByCity(city);
+
+		verify(beanMapperService).mapTo(airports, AirportDTO.class);
+		verify(airportService).findAirportsByCity(city);
+	}
+
+	/**
+	 * Test of getAirportsByName method, of class AirportFacadeImpl.
+	 */
+	@Test
+	public void testGetAirportsByName() {
+		System.out.println("getAirportsByName");
+		Airport airport = createAirport1();
+		AirportDTO airportDTO = createAirportDTO1();
+
+		List<Airport> airports = new ArrayList<>();
+		airports.add(airport);
+
+		String name = "Brno letiste";
+
+		when(beanMapperService.mapTo(airportDTO, Airport.class)).thenReturn(airport);
+		when(beanMapperService.mapTo(airport, AirportDTO.class)).thenReturn(airportDTO);
+		when(airportService.findAirportsByName(name)).thenReturn(airports);
+
+		List<AirportDTO> result = airportFacade.getAirportsByName(name);
+
+		verify(beanMapperService).mapTo(airports, AirportDTO.class);
+		verify(airportService).findAirportsByName(name);
+	}
+
+	/**
+	 * Test of getAirportsByCountry method, of class AirportFacadeImpl.
+	 */
+	@Test
+	public void testGetAirportsByCountry() {
+		System.out.println("getAirportsByCountry");
+		Airport airport = createAirport1();
+		AirportDTO airportDTO = createAirportDTO1();
+
+		List<Airport> airports = new ArrayList<>();
+		airports.add(airport);
+
+		String country = "Czech republic";
+
+		when(beanMapperService.mapTo(airportDTO, Airport.class)).thenReturn(airport);
+		when(beanMapperService.mapTo(airport, AirportDTO.class)).thenReturn(airportDTO);
+		when(airportService.findAirportsByCountry(country)).thenReturn(airports);
+
+		List<AirportDTO> result = airportFacade.getAirportsByCountry(country);
+
+		verify(beanMapperService).mapTo(airports, AirportDTO.class);
+		verify(airportService).findAirportsByCountry(country);
+	}
+
+	/**
+	 * Test of removeAirport method, of class AirportFacadeImpl.
+	 */
+	@Test
+	public void testRemoveAirport() {
+		System.out.println("removeAirport");
+		
+	}
+
+	/**
+	 * Test of updateAirport method, of class AirportFacadeImpl.
+	 */
+	@Test
+	public void testUpdateAirport() {
+		System.out.println("updateAirport");
+		Airport airport = createAirport1();
+		AirportDTO airportDTO = createAirportDTO1();
+
+		when(beanMapperService.mapTo(airportDTO, Airport.class)).thenReturn(airport);
+		when(beanMapperService.mapTo(airport, AirportDTO.class)).thenReturn(airportDTO);
+		when(airportService.updateAirport(airport)).thenReturn(airport);
+
+		AirportDTO created = airportFacade.updateAirport(airportDTO);
+		assertEquals(airportDTO, created);
+	}
+	
+}
