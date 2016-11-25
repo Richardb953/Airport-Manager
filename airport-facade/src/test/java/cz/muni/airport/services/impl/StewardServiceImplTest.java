47277ff80cec14285d9cb26e0287dda0761fbc31
Karolína Božková <karolina.bozkova@gmail.com>
2016-11-26 00:13:30 +0100

    test completed

--- airport-facade/src/test/java/cz/muni/airport/services/impl/StewardServiceImplTest.java
+++ airport-facade/src/test/java/cz/muni/airport/services/impl/StewardServiceImplTest.java
@@ -1,8 +1,12 @@
 package cz.muni.airport.services.impl;
 
 import cz.muni.airport.dao.StewardDAO;
+import cz.muni.airport.model.Airport;
+import cz.muni.airport.model.Flight;
 import cz.muni.airport.model.Steward;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -32,6 +36,154 @@ public class StewardServiceImplTest {
         steward.setFirstName("John");
         steward.setLastName("Malkovich");
         return steward;
+    }
+    private List<Airport> prepareAirports(){
+        Airport turany = new Airport();
+        turany.setCity("Brno");
+        turany.setCountry("CZE");
+        turany.setIata("BRQ");
+        turany.setName("Tuřany");
+        
+        Airport blava = new Airport();
+        blava.setCity("Bratislava");
+        blava.setCountry("SLO");
+        blava.setIata("BTS");
+        blava.setName("Blava");
+        
+        Airport paris = new Airport();
+        paris.setCity("Paris");
+        paris.setCountry("FRA");
+        paris.setIata("CDG");
+        paris.setName("Charles de Gaull");
+        
+        Airport london = new Airport();
+        london.setCity("London");
+        london.setCountry("GBR");
+        london.setIata("LHR");
+        london.setName("Heathrow");
+        
+        List<Airport> airports = new ArrayList<>(4);
+        airports.add(0, turany);
+        airports.add(1, blava);
+        airports.add(2, paris);
+        airports.add(3, london);
+        /*
+        Airport brno = prepareAirports().get(0);
+        Airport blava = prepareAirports().get(1);
+        Airport paris = prepareAirports().get(2);
+        Airport london = prepareAirports().get(3);
+        */
+        
+        return airports;
+    }
+    
+    private Date createDate(int year, int month, int day, int hour, int minute){
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month, day, hour, minute);
+        return cal.getTime();
+    }
+    
+    private Flight prepareFlight(){
+        Airport brno = prepareAirports().get(0);
+        Airport london = prepareAirports().get(3);
+        
+        Flight flight = new Flight();
+        flight.setName("Wants airplane");
+        flight.setDestinationPort(brno);
+        flight.setSourcePort(london);
+        flight.setDeparture(createDate(2016, Calendar.JANUARY, 1, 8, 0));
+        flight.setArrival(createDate(2016, Calendar.JANUARY, 1, 11, 0));
+        
+        return flight;
+        
+    }
+    
+    private List<Flight> prepareUnavailableFlights(){
+        Airport brno = prepareAirports().get(0);
+        Airport blava = prepareAirports().get(1);
+        Airport paris = prepareAirports().get(2);
+        Airport london = prepareAirports().get(3);
+        
+        Flight interferesTimely = new Flight();
+        interferesTimely.setName("the same time");
+        interferesTimely.setDestinationPort(brno);
+        interferesTimely.setSourcePort(london);
+        interferesTimely.setArrival(createDate(2016, Calendar.JANUARY, 1, 11, 0));
+        interferesTimely.setDeparture(createDate(2016, Calendar.JANUARY, 1, 9, 0));
+        
+        Flight interferesDestination = new Flight();
+        interferesDestination.setName("the same time");
+        interferesDestination.setDestinationPort(paris);
+        interferesDestination.setSourcePort(london);
+        interferesDestination.setArrival(createDate(2016, Calendar.JANUARY, 1, 7, 0));
+        interferesDestination.setDeparture(createDate(2016, Calendar.JANUARY, 1, 2, 0));
+        
+        Flight interferesSource = new Flight();
+        interferesSource.setName("the same time");
+        interferesSource.setDestinationPort(paris);
+        interferesSource.setSourcePort(london);
+        interferesSource.setArrival(createDate(2016, Calendar.JANUARY, 1, 15, 0));
+        interferesSource.setDeparture(createDate(2016, Calendar.JANUARY, 1, 12, 0));
+        
+        List<Flight> unavailable = new ArrayList<Flight>(3);
+        unavailable.add(0,interferesTimely);
+        unavailable.add(1,interferesDestination);
+        unavailable.add(2,interferesSource);
+        return unavailable;
+        
+        
+    }
+    
+    private List<Flight> prepareAvailableFlights(){
+        Airport brno = prepareAirports().get(0);
+        Airport blava = prepareAirports().get(1);
+        Airport paris = prepareAirports().get(2);
+        Airport london = prepareAirports().get(3);
+        
+        Flight previous = new Flight();
+        previous.setName("the same time");
+        previous.setDestinationPort(london);
+        previous.setSourcePort(brno);
+        previous.setArrival(createDate(2016, Calendar.JANUARY, 1, 7, 0));
+        previous.setDeparture(createDate(2016, Calendar.JANUARY, 1, 2, 0));
+        
+        Flight next = new Flight();
+        next.setName("the same time");
+        next.setDestinationPort(paris);
+        next.setSourcePort(brno);
+        next.setArrival(createDate(2016, Calendar.JANUARY, 1, 15, 0));
+        next.setDeparture(createDate(2016, Calendar.JANUARY, 1, 12, 0));
+        
+         List<Flight> available = new ArrayList<Flight>(2);
+        available.add(0,previous);
+        available.add(1,next);
+        return available;
+        
+        
+    }
+    
+   
+    @Test
+    public void testIsAvailableEmpty() {
+        List<Flight> empty = new ArrayList<>();
+        Steward steward = prepareSteward1();
+        steward.setFlights(empty);
+        
+        assertTrue(stewardService.isAvailable(steward, prepareFlight()));    
+    }
+    
+    @Test
+    public void testIsAvailable() {
+        List<Flight> unavailable = prepareUnavailableFlights();
+        List<Flight> available = prepareAvailableFlights();
+               
+        Steward steward = prepareSteward1();
+        
+        steward.setFlights(available);
+        assertTrue(stewardService.isAvailable(steward, prepareFlight()));
+        
+        steward.setFlights(unavailable);
+        assertTrue(!stewardService.isAvailable(steward, prepareFlight()));
     }
 
     /**
