59fcd2e7f72611147053c1fad02613851ef5591d
Richardb953 <richardbariny@gmail.com>
2016-11-25 22:14:13 +0100

    testy

---
+++ airport-facade/src/test/java/cz/muni/airport/facade/AirplaneFacadeTestImpl.java
@@ -0,0 +1,9 @@
+package cz.muni.airport.facade;
+
+/**
+ * Created by Richard Bariny on 25.11.2016.
+ *
+ * @author Richard Bariny, github name:Richardb953
+ */
+public class AirplaneFacadeTestImpl {
+}
