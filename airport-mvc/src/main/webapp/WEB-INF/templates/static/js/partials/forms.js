6aa18edcf3ebf62ba3ef6ebbf162af2c5247cdc8
Andrea N <andy.navratilova@gmail.com>
2016-12-20 23:50:22 +0100

    templates for all entities (https://github.com/andrea-n/airport-manager-frontend)

---
+++ airport-mvc/src/main/webapp/WEB-INF/templates/static/js/partials/forms.js
@@ -0,0 +1,53 @@
+$(document).ready(function ($) {
+
+	$('.btn-update').each(function() {
+		$(this).on('click', function() {
+			var form = $('#update');
+
+			if(form.length>0) {
+				if(!form.hasClass('active')) {
+					form.fadeIn('fast');
+					form.addClass('active');
+				}
+
+				var row = $(this).closest('tr'),
+					columns = row.find('td');
+				columns.each(function() {
+					var name = $(this).data('name'),
+						input = form.find('#' + name),
+						type = $(this).data('type');
+					if(input.length > 0) {
+						if(type!=undefined && type=="select") {
+							var option = $(this).data('option');
+							input.val(option);
+						}
+						else {
+							input.val($(this).text().trim());
+						}
+					}
+				});
+			}
+		});
+	});
+
+	$('.btn-cancel').each(function() {
+		$(this).on('click', function() {
+			var form = $(this).closest('form');
+			form.find(".form-control").val("");
+			$(this).closest('.hidden').fadeOut('fast').removeClass('active');
+		});
+	});
+
+	$("#add-form").validate({
+		submitHandler: function(form) {
+			form.submit();
+		}
+	});
+
+	$("#update-form").validate({
+		submitHandler: function(form) {
+			form.submit();
+		}
+	});
+
+});
