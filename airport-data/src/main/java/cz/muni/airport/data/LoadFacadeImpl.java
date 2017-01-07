9bfad35136c85caec8d13d8997595a9ad5510f12
Richardb953 <richardbariny@gmail.com>
2017-01-07 14:50:38 +0100

    pridate testovacie data

--- airport-data/src/main/java/cz/muni/airport/data/LoadFacadeImpl.java
+++ airport-data/src/main/java/cz/muni/airport/data/LoadFacadeImpl.java
@@ -1,6 +1,25 @@
 package cz.muni.airport.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import cz.muni.airport.model.Airplane;
+import cz.muni.airport.model.Airport;
+import cz.muni.airport.model.Flight;
+import cz.muni.airport.model.Steward;
+import cz.muni.airport.model.enums.FlightState;
+import cz.muni.airport.model.enums.PlaneType;
+import cz.muni.airport.services.AirplaneService;
+import cz.muni.airport.services.AirportService;
+import cz.muni.airport.services.FlightService;
+import cz.muni.airport.services.StewardService;
 
 /**
  * Created by Richard Bariny on 10.12.2016.
@@ -9,13 +28,132 @@ import java.io.IOException;
  * @author Richard Bariny, github name:Richardb953
  */
 
+
+@Component
+@Transactional
 public class LoadFacadeImpl implements LoadFacade {
+
+    @Autowired
+    AirplaneService airplaneService;
+
+    @Autowired
+    FlightService flightService;
+
+    @Autowired
+    AirportService airportService;
+
+    @Autowired
+    StewardService stewardService;
+
 
     @Override
     public void loadData() throws IOException {
-     //   Steward steward = new Steward();
-     //   steward.setLastName("a");
-     //   steward.setFirstName("v");
-       // stewardService.addSteward(steward);
+        Airplane airplane = createAirplane("BOEING-474", PlaneType.AIRLINER, 110);
+        Airplane airplane1 = createAirplane("BOEING-989", PlaneType.AIRLINER, 500);
+        Airplane airplane2 = createAirplane("Helicopter", PlaneType.HELICOPTER, 10);
+        Airplane airplane3 = createAirplane("MAC-555", PlaneType.TWIN_PISTON, 2);
+        Airplane airplane4 = createAirplane("CARGO-96", PlaneType.CARGO, 12);
+
+        Steward steward = createSteward("Anna", "Hlucha", null);
+        Steward steward1 = createSteward("Zuzka", "Presibana", null);
+        Steward steward2 = createSteward("Mirko", "Supko", null);
+        Steward steward3 = createSteward("Lojzo", "Mrtvy", null);
+        Steward steward4 = createSteward("Jana", "Vyhladavana", null);
+        Steward steward5 = createSteward("Richard", "Mojzo", null);
+        Steward steward6 = createSteward("Lucia", "Meliskova", null);
+        Steward steward7 = createSteward("Maria", "Polna", null);
+        Steward steward8 = createSteward("Janko", "Hrasko", null);
+        Steward steward9 = createSteward("Peter", "Marek", null);
+        Steward steward10 = createSteward("Marek", "Petrovic", null);
+        Steward steward11 = createSteward("Karolina", "Svetoznama", null);
+
+        Airport airport = createAirport("Dublin", "Dublin-888-aa-87", "Irsko", "Dublin");
+        Airport airport1 = createAirport("Bratislava - Stefanik", "Bratislava-8sf-a-2001", "Slovensko", "Bratislava");
+        Airport airport2 = createAirport("Kosicke hlavne", "Kosice-7k8-KK-17", "Slovensko", "Kosice");
+        Airport airport3 = createAirport("Londyn Heatway", "London-ENG-HH-87", "Anglicko", "Londyn");
+        Airport airport4 = createAirport("Berlin neuer", "Berlin-NN8-7a-82", "Nemecko", "Berlin");
+        Airport airport5 = createAirport("Vieden BaufHaufen", "Vien-VV11-b-81", "Rakusko", "Vieden");
+        Airport airport6 = createAirport("New York city", "NC-8cc-New-2017", "USA", "New York");
+        Airport airport7 = createAirport("Rio", "DeJanEro-188-SA-87", "Brazilia", "Rio");
+
+        List<Steward> stewardList = new ArrayList<Steward>();
+        stewardList.add(steward);
+        stewardList.add(steward1);
+        stewardList.add(steward2);
+        stewardList.add(steward3);
+
+        List<Steward> stewardList1 = new ArrayList<Steward>();
+        stewardList.add(steward4);
+        stewardList.add(steward5);
+        stewardList.add(steward6);
+        stewardList.add(steward7);
+
+        List<Steward> stewardList2 = new ArrayList<Steward>();
+        stewardList.add(steward8);
+        stewardList.add(steward9);
+        stewardList.add(steward10);
+        stewardList.add(steward10);
+
+        List<Steward> stewardList3 = new ArrayList<Steward>();
+        stewardList.add(steward);
+        stewardList.add(steward5);
+        stewardList.add(steward8);
+        stewardList.add(steward10);
+
+        Flight flight = createFlight(toDate(2017, 11, 31, 22, 0),toDate(2018, 0, 1, 1, 10), "Vianocny utocny Let Nemecko", airport, airport3, airplane3, 1, stewardList, FlightState.ACCEPTED);
+        Flight flight1 = createFlight(toDate(2017, 1, 1, 10, 20),toDate(2017, 1, 2, 12, 30), "Cestovny Let Vieden", airport1, airport5, airplane, 69, stewardList, FlightState.IN_VERIFY);
+        Flight flight2 = createFlight(toDate(2017, 1, 1, 1, 30),toDate(2017, 1, 1, 23, 10), "normalny Let USA", airport4, airport6, airplane4, 50, stewardList, FlightState.DECLINED);
+        Flight flight3 = createFlight(toDate(2017, 1, 5, 3, 50),toDate(2017, 1, 7, 7, 45), "dialkovy Let Rio", airport2, airport7, airplane1, 478, stewardList, FlightState.OPEN);
+        Flight flight4 = createFlight(toDate(2017, 1, 5, 5, 55),toDate(2017, 1, 5, 6, 55), "kratky Let Slovensko", airport1, airport2, airplane2, 8, stewardList, FlightState.ACCEPTED);
+
+
+    }
+    private static Date toDate(int year, int month, int day, int hours, int minutes) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day, hours, minutes);
+        return calendar.getTime();
+    }
+
+
+    private Airplane createAirplane(String name, PlaneType planeType, int capacity){
+        Airplane airplane = new Airplane();
+        airplane.setName(name);
+        airplane.setType(planeType);
+        airplane.setCapacity(capacity);
+        return airplaneService.saveAirplane(airplane);
+    }
+
+    private Steward createSteward(String name, String last, ArrayList flights){
+        Steward steward = new Steward();
+        steward.setFirstName(name);
+        steward.setLastName(last);
+        if(flights == null){
+            flights = new ArrayList();
+        }
+        steward.setFlights(flights);
+        return stewardService.addSteward(steward);
+    }
+
+    private Airport createAirport(String name, String iata, String country, String city){
+        Airport airport = new Airport();
+        airport.setName(name);
+        airport.setIata(iata);
+        airport.setCountry(country);
+        airport.setCity(city);
+        return airportService.saveAirport(airport);
+    }
+
+    private Flight createFlight(Date arrival, Date departure, String name, Airport source, Airport destin, Airplane airplane, int passCount, List<Steward> stewardList, FlightState state){
+        Flight flight = new Flight();
+        flight.setArrival(arrival);
+        flight.setDeparture(departure);
+        flight.setName(name);
+        flight.setSourcePort(source);
+        flight.setDestinationPort(destin);
+        flight.setAirplane(airplane);
+        flight.setPassagers(passCount);
+        flight.setStewards(stewardList);
+        flight.setFlightState(state);
+        return flightService.saveFlight(flight);
     }
 }
