ce9ff919c2b93dc7f1c898aae8e5d77668f4f705
xkrejci7 <jirikr@gmail.com>
2017-01-11 15:11:49 +0100

    flight without steward, update/add form with select-option

--- airport-mvc/src/main/java/cz/muni/airport/mvc/controller/FlightController.java
+++ airport-mvc/src/main/java/cz/muni/airport/mvc/controller/FlightController.java
@@ -1,5 +1,6 @@
 package cz.muni.airport.mvc.controller;
 
+import cz.muni.airport.facadeApi.AirplaneFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import cz.muni.airport.facadeApi.AirportFacade;
 import cz.muni.airport.facadeApi.FlightFacade;
+import cz.muni.airport.facadeApi.StewardFacade;
 
 /**
  * Created by Richard Bariny on 10.12.2016.
  *
  * @author Richard Bariny, github name:Richardb953
  */
-
 @Controller
 @RequestMapping("/flight")
 @Transactional
@@ -26,6 +27,12 @@ public class FlightController {
 
     @Autowired
     private AirportFacade airportFacade;
+
+    @Autowired
+    private AirplaneFacade airplaneFacade;
+    
+    @Autowired
+    private StewardFacade stewardFacade;
 
     /**
      * Shows a list of products with the ability to add, delete or edit.
@@ -41,10 +48,11 @@ public class FlightController {
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public String flights(Model model) {
-		model.addAttribute("flights", airportFacade.getAllAirports());
+        model.addAttribute("flights", flightFacade.getAllFlights());
+        model.addAttribute("airplanes", airplaneFacade.getAllAirplanes());
+        model.addAttribute("stewards", stewardFacade.getAllStewards());
+        model.addAttribute("airports", airportFacade.getAllAirports());
         return "flights";
     }
-
-
 
 }
