2809ce7b5f8982c0897dc65a066088d5719170da
Karolína Božková <karolina.bozkova@gmail.com>
2017-01-12 19:26:52 +0100

    not complete

--- airport-mvc/src/main/java/cz/muni/airport/mvc/controller/AirplaneController.java
+++ airport-mvc/src/main/java/cz/muni/airport/mvc/controller/AirplaneController.java
@@ -1,5 +1,7 @@
 package cz.muni.airport.mvc.controller;
 
+import cz.muni.airport.facadeApi.AirplaneFacade;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -17,7 +19,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/airplane")
 @Transactional
 public class AirplaneController {
-
+    @Autowired
+    private AirplaneFacade airplaneFacade;
+    
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("airplanes", airplaneFacade.getAllAirplanes());
+        return "airplane/list";
+    }
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public String airplanes(Model model) {
