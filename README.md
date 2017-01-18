149318b16a95ec66b7bcaa7f70b4ac8683bf99d9
Karolína Božková <karolina.bozkova@gmail.com>
2017-01-18 13:52:04 +0100

    security config cleaned, readme updated

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Airport-Manager, a school project pro class PA165 Enterprise Java.
 
 ## Running Airport-Manager
-In folder Airport-Manager run:  `mvn clean install && tomcat7:run`    
+In folder Airport-Manager run:  `mvn clean install && mvn tomcat7:run`    
 Airport-manager runs on: [`http://localhost:8082/pa165`  ](http://localhost:8082/pa165)
 
 ## Login credentials
@@ -18,6 +18,47 @@ Manager has access to everything. Can create, update, delete all entities.
 Can view only Flights. Is able to create, update, delete flights with existing stewards, airplanes and airports. 
 
 ## Rest interface
+Runs on: [`http://localhost:8082/pa165/rest`](http://localhost:8082/pa165/rest)
+
+Project implements REST API for entities:
+* **Steward** and methods GET, PUT, POST, DELETE
+* **Airport** and methods GET, PUT, POST, DELETE
+* **Flight** and method GET
+* **Airplane** and method GET
+
+| Title        | URL           | Method |  Params  | Data params (JSON) |
+| ------------- |:------------:| :-----:|:--------:| :-----:|
+| Get all flights | `/flights/` | GET   |  |    |
+| Get all airplanes | `/airplanes/` | GET   |  |    |
+| Get all stewards | `/stewards/` | GET   |  |    |
+| Get steward | `/stewards/id/{id}` | GET   | Steward ID |    |
+| Get steward | `/stewards/name/{firstname}/{lastname}` | GET   | First name, Last name |    |
+| Delete steward | `/stewards/id/{id}` | DELETE | Steward ID |    |
+| Create new steward | `/stewards/create` | POST |  | {"firstName": "firstName", "lastName":"lastName", "flights":[]}   |
+| Update steward | `/stewards/id/{id}` | PUT | Steward ID | {"firstName": "firstNameUpdate", "lastName":"lastNameUpdate", "flights":[]}   |
+| Get all airports| `/airports/` | GET   |  |    |
+| Get airport | `/airports/id/{id}` | GET   | Airport ID |    |
+| Get airport | `/airports/city/{city}` | GET   | Airport city |    |
+| Get airport | `/airports/city/{country}` | GET   | Airport country |    |
+| Get airport | `/airports/city/{iata}` | GET   | Airport iata |    |
+| Get airport | `/airports/city/{name}` | GET   | Airport name |    |
+| Delete airport | `/airports/id/{id}` | DELETE | Airport ID |    |
+| Create new airport| `/airports/create` | POST |  | {"iata": "iata", "name": "name", "country": "country", "city": "city"}   |
+| Update airport | `/airports/id/{id}` | PUT | Airport ID | {"iata": "iata2", "name": "name2", "country": "country2", "city": "city2"}   |
+
+### Example usage commands:
+```
+curl -i -X GET http://localhost:8082/pa165/rest/airports
+```
+```
+curl -i -X DELETE http://localhost:8082/pa165/rest/stewards/id/12
+```
+```
+curl -X POST -i -H "Content-Type: application/json" --data '{"iata": "iata", "name": "name", "country": "country", "city": "city" }' http://localhost:8082/pa165/rest/airports/create
+```
+```
+curl -i -X GET http://localhost:8082/pa165/rest/stewards/name/Peter/Marek
+```
 
 ## Team members
 * Richard Bariny - (učo: 461431,Github name: [Richardb953](https://github.com/Richardb953)) - team leader
--- airport-mvc/src/main/java/cz/muni/airport/mvc/conf/SecurityConfig.java
+++ airport-mvc/src/main/java/cz/muni/airport/mvc/conf/SecurityConfig.java
@@ -3,97 +3,37 @@ package cz.muni.airport.mvc.conf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CsrfFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled=true)
-public class SecurityConfig {
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//            .authorizeRequests().anyRequest().authenticated()
-//                .antMatchers("/app/login/*").permitAll()
-//                .antMatchers("/", "/home", "/flight/**").access("hasAnyRole('ROLE_MANAGER', 'ROLE_CASHIER')")
-//                .antMatchers("/airplane/**", "/airport/**", "/steward/**").hasRole("MANAGER")
-//                .and().httpBasic().and()
-//            .formLogin()
-//                .loginPage("/app/login")
-//                .loginProcessingUrl("/appLogin")
-//                .usernameParameter("username")
-//                .passwordParameter("password")
-//                .defaultSuccessUrl("/")
-//                .and()
-//            .logout()
-//                .logoutUrl("/appLogout")
-//                .logoutSuccessUrl("/app/login")
-//                .and()
-//                .exceptionHandling().accessDeniedPage("/app/accessDenied");
-//        
-//    }
-    @Configuration
-    @Order(1)
-    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
-            protected void configure(HttpSecurity http) throws Exception {
-                    http
-//                            .antMatcher("airplane/*").antMatcher("/steward/*").antMatcher("/airport/*")
-//                            .antMatcher("airplane/update/*").antMatcher("/steward/update/*").antMatcher("/airport/update/*")
-//                            .antMatcher("airplane/delete/*").antMatcher("/steward/delete/*").antMatcher("/airport/delete/*")
-                            .authorizeRequests()
-                                    .anyRequest().hasRole("MANAGER").antMatchers("airplane/**","/steward/*","/airport/*").access("hasAnyRole('ROLE_MANAGER')")
-                                    .and();
-//                            .httpBasic();
-                    http
-                            .antMatcher("/flight/*")
-                            .authorizeRequests()
-                                    .anyRequest().hasAnyRole("MANAGER","CASHIER")
-                                    .and();
-                            //.httpBasic();
-            }
-    }
-
-    @Configuration
-    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-
-            @Override
-            protected void configure(HttpSecurity http) throws Exception {
-                    http.csrf().disable()
-                        .authorizeRequests()
-                            .antMatchers("/app/login", "/static/**", "/layouts/**").permitAll()
-                            .anyRequest().authenticated()
-                            .and()
-                        .formLogin()
-                            .loginPage("/app/login")
-                            .loginProcessingUrl("/appLogin")
-                            .usernameParameter("username")
-                            .passwordParameter("password")
-                            .defaultSuccessUrl("/")
-                            .and()
-                        .logout()
-                            .logoutUrl("/app/logout")
-                            .logoutSuccessUrl("/app/login")
-                            .and()
-                            .exceptionHandling().accessDeniedPage("/app/accessDenied").and();
-            }
-            
-            @Override
-            public void configure(WebSecurity web) throws Exception {
-                web.ignoring().antMatchers("/favicon.ico", "/static/**");
-            }
-            
-            @Bean
-            @Override
-            public AuthenticationManager authenticationManagerBean() throws Exception {
-                return super.authenticationManagerBean();
-            }
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/app/login", "/static/**", "/layouts/**", "/app/accessDenied", "/app/logout").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/app/login")
+                .loginProcessingUrl("/appLogin")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/")
+                .and()
+            .logout()
+                .logoutUrl("/app/logout")
+                .logoutSuccessUrl("/app/login")
+                .and()
+                .exceptionHandling().accessDeniedPage("/app/accessDenied").and();
+        
     }
 
     @Autowired
@@ -102,10 +42,10 @@ public class SecurityConfig {
         auth.inMemoryAuthentication().withUser("cashier").password("cashier").roles("CASHIER");
     }
     
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 	
 }
